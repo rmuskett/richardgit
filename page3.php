75171f5473a92c18dc79e796399350424b6a6ffd
WayneLN <waynedk@btinternet.com>
2013-02-19 12:33:11 +0000

    These files committed in the branch - branching
    	new file:   images/branching.png
    	new file:   images/head.png
    	new file:   images/head2.png
    	new file:   images/head3.png
    	modified:   page3.php

--- page3.php
+++ page3.php
@@ -17,19 +17,71 @@ include_once('header.php');
 
 		<br /><br />
 
-		<p>WORK IN PROGRESS:</p>
+		<p>Branching is a difficult concept to explain, but very powerful
+		and easy to use once you've grasped the basics. Essentially
+		it lets you divert the commit path, leaving intact the master
+		branch, which you can return too at any time. The point of 
+		divergance is recorded and you can flip back at will. The most 
+		dramatic effect can be seen by deleting a file in a branch which
+		will remove it from the working directory, you can see this in your
+		code editor. If you now flip back to the master branch the file
+		will magically re-appear in your working directory. Flip between
+		the branches as watch the file appear and disappear.</p>
 
-		<p>There are two other processes we need to cover, which don't 
-		quite fit into the loop concept. The first is getting you local 
-		repository setup in the first place and the second is pushing 
-		work up from the Remote Master to the LIVE production 
-		environment.</p>
+		<p>Of course the file is not really being deleted, GiT is simply
+		hiding the file which you deleted in the branch. This change effect
+		is applied to any changes, GiT knows the status of all the files in
+		both branches. This even applies to testing the files. If the branch is
+		active (checked out) then  then testing on the working directory
+		will respond to the branch you are in.</p> 
 
-		<br /><br /><br /><br /><br /><br /><br /><br /><br />
-		<br /><br /><br /><br /><br /><br /><br /><br /><br />
-		<br /><br /><br /><br /><br /><br /><br /><br /><br />
-		<br /><br /><br /><br /><br /><br /><br /><br /><br />
-		<br /><br /><br /><br /><br /><br /><br /><br /><br />
+		<p>You can open as many branches as you want, they will all act
+		independently of each other, until you MERGE them. Ultimately
+		you will want to merge them into the master branch and push up
+		to your remote. </p>
+
+		<p>Now to try and explain this graphically. So let's have two branches
+		Master and Testing.</p>
+		
+		<img src="images/branching.png">
+
+		<br /><br />
+
+		<p>Above we have three commits, at the point of commit f30ab, we
+		have created a new branch named testing. All we have done so far is
+		created a new pointer, we have made another commit so nothing has
+		diverged yet.</p>
+
+		<img src="images/head.png">
+
+		<br /><br />
+
+		<p>HEAD is the pointer, this is where GiT is pointing too. To
+		swap to another branch you 'checkout' to that branch this moves HEAD
+		to 'testing' in this case.</p>
+
+		<img src="images/head2.png">
+
+		<br /><br />
+
+		<p>Now when we do more editing and then make another commit, HEAD moves
+		on in the testing branch, but not the pointer to master remains
+		in place, so when you swap back to master (checkout master) the
+		working directory will revert to that status. master will reflect
+		the working directory's status before any changes you made in commit
+		2cb9e. Both master and testing will stay separated, until you merge
+		testing with master or discard it</p>
+
+		<img src="images/head3.png">
+
+		<br /><br />
+
+		<p>There is lots more to branching and how this technique can be used.
+		here's a link to the ProGiT chapter whcih covers this in much more depth,
+		<a href="http://git-scm.com/book/en/Git-Branching-What-a-Branch-Is">Branching and Merging</a> 
+		but I hope this is enough to get you started with the concept. The
+		images above are adapted from ProGiT</p>
+
 
 	</div>   <!-- end of mainContent -->
 
