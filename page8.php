2b234793f1f4bc0a00ebbb676247e975aad411a9
Richard <richard.muskett@southeasternrailway.co.uk>
2013-04-10 16:14:02 +0100

    My first git upload

---
+++ page8.php
@@ -0,0 +1,51 @@
+<?php
+
+include_once('header.php');
+
+
+?>
+
+<body>
+
+<div id='wrapper'>	
+
+<div id='mainDiv' class='gradient'>
+
+<div id='mainContent'>
+
+		<h1>Cloning Down From GitHub</h1>
+
+		<br /><br />
+
+		<p>my first test page</p>
+
+
+
+
+		
+
+	
+		<br /><br /><br /><br /><br /><br /><br /><br /><br />
+		<br /><br /><br /><br /><br /><br /><br /><br /><br />
+		<br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+	</div>   <!-- end of mainContent -->
+
+	<div id='sideBar'>		
+
+		<h3>Cloning From GitHub</h3>
+
+	
+
+
+	<?php include_once('menu.php'); ?>
+			
+	</div> <!-- end of sideBar -->
+
+
+<div id='footGap'></div>
+
+<?php
+include_once('footer.php');
+?>
+
